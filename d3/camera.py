29ec3e1a3d17376a986c2eae7505a0689af329f7
Thomas FORGIONE <thomas.forgione@gmail.com>
2017-01-18 14:01:17 +0100

    Cleaned camera

--- d3/camera.py
+++ d3/camera.py
@@ -1,17 +1,27 @@
 from .geometry import Vector
 
-from OpenGL.GL import *
-from OpenGL.GLU import *
-
+import OpenGL.GLU as glu
 
 class Camera:
-    def __init__(self, position = None, target = None, up = None):
-        self.position = Vector() if position is None else position
-        self.target = Vector() if target is None else target
-        self.up = Vector(0.0,1.0,0.0) if up is None else target
+    """Simple 3D camera
+    """
+    def __init__(self, position = Vector(1.0,0.0,0.0), target = Vector(), up = Vector(0.0,1.0,0.0)):
+        """Creates a simple camera
+
+        :param position: center of the camera
+        :param target: point where the camera is looking
+        :param up: up vector of the camera
+        """
+        self.position = position
+        self.target = target
+        self.up = up
 
     def look(self):
-        gluLookAt(
+        """Sets the model view matrix of OpenGL
+
+        Simply calls the gluLookAt function
+        """
+        glu.gluLookAt(
             self.position.x, self.position.y, self.position.z,
             self.target.x, self.target.y, self.target.z,
             self.up.x, self.up.y, self.up.z)
